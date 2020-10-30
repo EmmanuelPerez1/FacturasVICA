9fdc1e39943f5bc4ec8adde6d7b46950447d24ac
EmmanuelPerez1 <arreolae475@gmail.com>
2020-10-30 17:31:03 -0600

    Agregado la búsqueda de otro tipo de OC, tambien se evita que la página se recargue despues de apretar el botón de extraer, se agrega el botón de borrado, y evita por el momento que marque error cuando no hay NUMEOC o Condiciones de Pago

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 document.getElementById('readerXML').addEventListener('submit', function(e) {
-
+    e.preventDefault();
     const xml = document.getElementById('xmlText').value;
-    // var UUID = /UUID\=\"\w\"?/;
-    // var UUID = /\w?<=(UUID\=\")/;
+
     var UUID = /([a-zA-Z0-9]+)-([a-zA-Z0-9]+)-([a-zA-Z0-9]+)-([a-zA-Z0-9]+)-([a-zA-Z0-9]+)/;
     var fechaTimbrado = /(\d{4})-(\d{2})-(\d{2})/;
     var subtotal = /SubTotal="(\d+(\.\d+))/;
     var total = / Total="(\d+(\.\d+))/;
     var NUMEOC = /NUMEOC  (\d+)/i;
-    var condicionesDePago = /CondicionesDePago =(\d+)-(\d+)/;
+    var condicionesDePago = /CondicionesDePago ="([a-zA-Z0-9]+)-([a-zA-Z0-9]+)/;
 
     var resultUUID = UUID.exec(xml);
     var resultfT = fechaTimbrado.exec(xml);
@@ -17,17 +16,24 @@ document.getElementById('readerXML').addEventListener('submit', function(e) {
     var resultOC = NUMEOC.exec(xml);
     var resultcdP = condicionesDePago.exec(xml);
 
-    // console.log(resultUUID[0]);
-    // console.log(resultfT[0])
-    // console.log(resultsT[0].replace('SubTotal="', ''));
-    // console.log(resultT[0].replace(' Total="', ''));
-    // console.log(resultOC[0].replace('NUMEOC  ', ''));
-
     document.getElementById('resultUUID').innerHTML = `${resultUUID[0]}`;
     document.getElementById('fechaTimbrado').innerHTML = `${resultfT[0]}`;
     document.getElementById('subTotal').innerHTML = `${resultsT[0].replace('SubTotal="', '')}`;
     document.getElementById('Total').innerHTML = `${resultT[0].replace(' Total="', '')}`;
-    document.getElementById('NUMEOC').innerHTML = `${resultOC[0].replace('NUMEOC  ', '')}`;
-    document.getElementById('condicionesDePago').innerHTML = `${resultcdP.replace('CondicionesDePago = ', '')}`;
-    e.preventDefault();
+    if (!resultOC == null) {
+        document.getElementById('NUMEOC').innerHTML = `${resultOC[0].replace('NUMEOC  ', '')}`;
+    }
+
+    if (!resultcdP == null) {
+        document.getElementById('condicionesDePago').innerHTML = `${resultcdP[0].replace('CondicionesDePago = ', '')}`;
+    }
 })
+
+function limpiarTexto() {
+    document.getElementById('xmlText').value = '';
+}
+
+// function seleccionarTexto(id_seleccionado) {
+//     id_seleccionado.value.select();
+//     document.execCommand('copy');
+// }
